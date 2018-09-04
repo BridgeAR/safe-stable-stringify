7a874787572eb373f6ce8d9c547ddc53165c832e
Andy Darr <andrew.darr86@gmail.com>
2018-09-04 11:10:33 -0400

    Update typescript signature for stringify method (#1)
    * updating typescript signature of stringify method to reflect the extra parameters
    
    * adding overload for stringify

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
-declare function stringify(data: any): string;
+declare function stringify(value: any, replacer?: (key: string, value: any) => any, space?: string | number): string;
+declare function stringify(value: any, replacer?: (number | string)[] | null, space?: string | number): string;
 
 export default stringify;
